c87450c52d5169dc3ac6516b09eaa7250cda39c8
SuelenCbrl <suelen.cbrl.sc@gmail.com>
2025-03-07 08:49:06 -0400

    atualização banco

--- APP/CLASSE/guiche.php
+++ APP/CLASSE/guiche.php
@@ -6,15 +6,5 @@ class guiche{
     public string $num_guiche;
     public string $nome_guiche;
 
-    public function cadastrar_guiche(){
-        $db= new Database('guiche');
-        $res = $db->insert(
-            [
-                'identificador'=>$this->num_guiche,
-                'nomeGuiche'=>$this->nome_guiche
-            ]
-            );
-        return $res;
-    }
 
 }
