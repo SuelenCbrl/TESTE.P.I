1a9a73e6b10404e081c325fdd6c656317f97e001
SuelenCbrl <suelen.cbrl.sc@gmail.com>
2025-03-10 08:31:16 -0400

    atualixzação

--- excluir_guiche.php
+++ excluir_guiche.php
@@ -1,13 +1,11 @@
 <?php
 require './APP/CLASSE/guiche.php';
-if(isset($_GET['id_guiche'])){
-    $id = $_GET['id_guiche'];
+$id_guiche = isset($_GET['id_guiche']) ? $_GET['id_guiche'] : null;
     $objUser = new Guiche();
     $user_delete = $objUser->buscar_por_id($id_guiche);
     $del= $user_delete->excluir();
     if($del){
         header('location: ./index.php');
     }
-}
-?>
 
+?>
